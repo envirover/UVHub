8ca8e003905d409bfc76c30cd9daf184405b6a6a
Pavel Bobov <pb@envirover.com>
2017-12-20 22:21:32 -0800

    Changed license in the source files headers

--- NVI/src/main/java/com/envirover/mavlink/MAVLinkMessageQueue.java
+++ NVI/src/main/java/com/envirover/mavlink/MAVLinkMessageQueue.java
@@ -1,26 +1,19 @@
 /*
-This file is part of SPLGroundControl application.
-
-SPLGroundControl is a ground control proxy station for ArduPilot rovers with
-RockBLOCK satellite communication.
-
-See http://www.rock7mobile.com/downloads/RockBLOCK-Web-Services-User-Guide.pdf
-
-Copyright (C) 2017 Envirover
-
-SPLGroundControl is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-SPLGroundControl is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with SPLGroundControl.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Envirover confidential
+ * 
+ *  [2017] Envirover
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains the property of 
+ * Envirover and its suppliers, if any.  The intellectual and technical concepts
+ * contained herein are proprietary to Envirover and its suppliers and may be 
+ * covered by U.S. and Foreign Patents, patents in process, and are protected
+ * by trade secret or copyright law.
+ * 
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Envirover.
+ */
 
 package com.envirover.mavlink;
 
