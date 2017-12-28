e4a79ee388815f4012afe491a08e615b508a68e1
Pavel Bobov <pb@envirover.com>
2017-12-27 23:38:50 -0800

    Added nvi.bat and nvi.sh

---
+++ bin/nvi.bat
@@ -0,0 +1,9 @@
+@echo off
+
+if "%SPL_HOME%"=="" (
+  echo Environment variable NVI_HOME is not set.
+  exit /b
+)
+
+set CLASSPATH=%NVI_HOME%\conf;%NVI_HOME%\lib\*
+java -cp "%CLASSPATH%" com.envirover.nvi.NVIGroundControl %*
