69d1535a188cc2227497bffc44ca492071c8f635
Pavel Bobov <pb@envirover.com>
2018-04-03 21:21:36 -0700

    envirover/support#1 Added uvhub.template

---
+++ distribution/bin/uvhub.bat
@@ -0,0 +1,9 @@
+@echo off
+
+if "%UVHUB_HOME%"=="" (
+  echo Environment variable UVHUB_HOME is not set.
+  exit /b
+)
+
+set CLASSPATH=%UVHUB_HOME%\conf;%UVHUB_HOME%\lib\*
+java -cp "%CLASSPATH%" com.envirover.uvhub.UVHub %*
