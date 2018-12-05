075d248f34e29b2ad26f9e2c157c11166cce9cca
Pavel Bobov <pb@envirover.com>
2018-12-04 21:29:00 -0800

    Fixed config.js

--- UVCockpit/WebContent/js/config.js
+++ UVCockpit/WebContent/js/config.js
@@ -2,7 +2,7 @@
 
 var UVTRACKS_PROTOCOL = location.protocol;
 var UVTRACKS_HOSTNAME = location.hostname;
-var UVTRACKS_PORT = ":8080";//location.port ? ':' + location.port : '';
+var UVTRACKS_PORT = location.port ? ':' + location.port : '';
 // Base URL of UV Tracks web service
 var UVTRACKS_BASE_URL = UVTRACKS_PROTOCOL + '//' + UVTRACKS_HOSTNAME + UVTRACKS_PORT;
 
